420eadd3882e2210e348b0c5d0dbab2ddf2badcd
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-18 14:41:21 +0100

    ajout des clonnes dans la table loans

--- gestionBiblio/database/migrations/2025_02_18_132240_create_loans_table.php
+++ gestionBiblio/database/migrations/2025_02_18_132240_create_loans_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign Key vers users(id)
+            $table->foreignId('book_id')->constrained()->onDelete('cascade'); // Foreign Key vers books(id)
+            $table->timestamp('borrowed_at')->default(now()); // date d'emprunt du livre 
+            $table->timestamp('returned_at')->nullable(); // date de retour du livre 
             $table->timestamps();
         });
     }
