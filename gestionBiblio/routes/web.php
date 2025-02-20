50c8efc11cbfeb74d551533ca2a075981737bfa2
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-20 14:39:21 +0100

    ajout des routes de redirection vers le vues de sign up et log in

--- gestionBiblio/routes/web.php
+++ gestionBiblio/routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SignUpController;
+use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signup', [SignUpController::class, 'show']);
+Route::get('/login', [LoginController::class, 'show']);
+
+
