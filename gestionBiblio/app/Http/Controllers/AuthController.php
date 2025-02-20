eead6b92f13c69a94d1fde72fbf9134513a77c39
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-20 16:37:50 +0100

    ajout de la fonction register dans le fichier authcontroller

--- gestionBiblio/app/Http/Controllers/AuthController.php
+++ gestionBiblio/app/Http/Controllers/AuthController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function showSinUp()
+    {
+        return view('signup');
+    }
+
+    public function showLogin()
+    {
+        return view('login');
+    }
+
+    // Gère l'inscription
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard')->with('success', 'Inscription réussie !');
+    }
+
+  
 }
+
