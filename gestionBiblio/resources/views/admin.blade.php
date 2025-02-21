e87d5348ec613aec029de3cff6944c29e6c281e0
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-21 10:17:20 +0100

    insertion du contenu de la page admin

---
+++ gestionBiblio/resources/views/admin.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Gestion des Livres</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100">
+
+    <!-- Barre de navigation -->
+    <nav class="bg-white shadow-md p-4">
+        <div class="max-w-6xl mx-auto flex justify-between items-center">
+            <h1 class="text-xl font-bold text-gray-700">📚 Book Dashboard</h1>
+            <button onclick="openModal()" class="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-600 transition">
+                + Ajouter un Livre
+            </button>
+        </div>
+    </nav>
+
+
+
+
+</body>
+
+
+</html>
