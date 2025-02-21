97fdbbf55706e2cd92eb5670a4ffbadaf6e3f83c
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-21 10:10:52 +0100

    ajout du route de redirection vers la page admin

--- gestionBiblio/routes/web.php
+++ gestionBiblio/routes/web.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
+
+// home route
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Auth routes
 Route::get('/signup', [AuthController::class, 'showSinUp']);
 Route::get('/login', [AuthController::class, 'showLogin']);
 Route::post('/signup', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Crud routes
+Route::get('/dashboard', [BookController::class, 'index'])->name('dashboard');
