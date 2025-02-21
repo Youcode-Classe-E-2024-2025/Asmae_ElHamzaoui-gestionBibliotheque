b33a9a03b18d4eac048d5377b04161d7918a4923
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-02-21 14:55:48 +0100

    page pour que les utilisateurs empruntent des livres

---
+++ gestionBiblio/resources/views/emprunt.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Affichage des alertes -->
+     @if(session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @elseif(session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
+ 
+
+</body>
+</html>
